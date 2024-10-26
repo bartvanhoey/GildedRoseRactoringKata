082212feb38f8c079c6f198d1bf767e4a06f4ec3
ekapituro <ekapituro@gmail.com>
2024-10-26 10:21:48 +0200

    update

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -18,33 +18,27 @@ public class GildedRoseTest
         var items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
+
         items[0].Name.Should().Be("foo");
         items[0].SellIn.Should().Be(-1);
         items[0].Quality.Should().Be(0);
+        
     }
     
     [Fact]
     public Task Approval_Test_Item_Should_Return_Expected_Values()
     {
-        string[] names = ["foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" ];
-        var sellIn = 0;
-        var quality = 0;
-        
-        return Verify(GetItems(names, sellIn, quality));
-    }
-
-    private static List<Item> GetItems(string[] name, int sellin, int quality)
-    {
-
-        var items = new List<Item>();
-        foreach (var se in name)
-        {
-            items.Add(new() { Name = se, SellIn = sellin, Quality = quality });    
-        }
-        
-        
+        var items = new List<Item> { new() { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        return items;
+
+        // items[0].Name.Should().Be("foo");
+        // items[0].SellIn.Should().Be(-1);
+        // items[0].Quality.Should().Be(0);
+        
+       return Verifier.Verify(items);
+                
     }
+
+
 }
