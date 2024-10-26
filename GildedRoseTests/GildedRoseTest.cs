b520e30fafb7d5a6fb49c060e7866b87c33a52e7
ekapituro <ekapituro@gmail.com>
2024-10-26 18:33:06 +0200

    Update

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -22,25 +22,11 @@ public class GildedRoseTest
         items[0].Name.Should().Be("foo");
         items[0].SellIn.Should().Be(-1);
         items[0].Quality.Should().Be(0);
-        
     }
     
-    [Fact]
-    public Task Approval_Test_Item_Should_Return_Expected_Values()
-    {
-        string[] names = ["foo"];
+    
+    
+    
 
-        var items = GetItemsForVerifying(names[0]);
-        
-        return Verifier.Verify(items);
-                
-    }
-
-    private static List<Item> GetItemsForVerifying(string name)
-    {
-        var items = new List<Item> { new() { Name = name, SellIn = 0, Quality = 0 } };
-        var app = new GildedRose(items);
-        app.UpdateQuality();
-        return items;
-    }
+    
 }
