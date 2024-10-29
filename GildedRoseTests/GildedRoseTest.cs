2693d31ec1df8e54e31568c884b7cba4e7927afa
ekapituro <ekapituro@gmail.com>
2024-10-29 18:06:59 +0100

    Update

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,19 +26,29 @@ public class GildedRoseTest
 
 
     [Fact]
-    public Task Approve_Items_Should_Return_Expected_Values()
+    public Task GildedRose_UpdateQuality_Should_Return_Correct_Values()
     {
         string[] names = ["foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" ];
         int[] qualities = [0,-1,1,49,50,51];
         int[] sellIns = [100,5,6,10,11,0,-1];
-        
-        return VerifyCombinations(DoStuff, names, qualities, sellIns);
+        var random = new Random();
+        var next = random.Next(1,3);
+        return next == 1 ? VerifyCombinations(GildedRoseOriginalQualityUpdater, names, qualities, sellIns) : VerifyCombinations(GildedRoseQualityUpdater, names, qualities, sellIns);
     }
 
-    private static string DoStuff(string name, int quality, int sellIn)
+    private static string GildedRoseQualityUpdater(string name, int quality, int sellIn)
     {
         var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
         var app = new GildedRose(items);
+        app.UpdateQuality();
+        var item = items[0];
+        return $"{item.Name}#{item.SellIn}#{item.Quality}";
+    }
+    
+    private static string GildedRoseOriginalQualityUpdater(string name, int quality, int sellIn)
+    {
+        var items = new List<Item> { new() { Name = name, SellIn = sellIn, Quality = quality } };
+        var app = new GildedRoseOriginal(items);
         app.UpdateQuality();
         var item = items[0];
         return $"{item.Name}#{item.SellIn}#{item.Quality}";
